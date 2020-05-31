32bc41ac7e0d0a2ab804150189e572186c9ef4ed
Horus Lugo <horusgoul@gmail.com>
2020-05-31 12:35:13 +0200

    feat: initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Hello World!</title>
+      </Head>
+      Hello World!
+    </>
+  );
+}
